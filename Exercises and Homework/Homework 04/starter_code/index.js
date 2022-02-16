92085b127f9eda1be8ece35033185891a70454ec
Lead-Way <liwei.hsu19.com>
2022-02-16 13:52:00 -0500

    homework WIP 2

--- Exercises and Homework/Homework 04/starter_code/index.js
+++ Exercises and Homework/Homework 04/starter_code/index.js
@@ -1,17 +1,23 @@
 // Level 1
 
-// define what are scores and ball
+// define what are score and ball element on the page. I don't understand why using El tho
 let score = 0; 
-let ballEl = document.getElemquerySelectorentsByClassName ("js-ball");
+let ballEl = document.getElementsByClassName ("js-ball");
 let scoreEl = document.getElementsByClassName ("js-score");
+
+// let ballEl = document.getElementsByClassName("js-ball");
+console.log(ballEl)
+
+let ballEl1 = document.querySelector(".js-ball");
+console.log(ballEl1)
 
 //define how the click is going to funtion
 ballEl.addEventListener("click", function() {
     // define click will increase scores by 10
     score += 10;
-    // dedine what condition should declare a winner. If the scores are over 100 then you decleare the winner
+    // define what condition should declare a winner. If the scores are over 100 then you decleare the winner
     if (score < 100) {
-    scoreEL.innertext = score;
+    scoreEL.innerText = score;
     } else {
     declareWinner();
     }
@@ -20,6 +26,7 @@ ballEl.addEventListener("click", function() {
 // Define what fuction of decleare winner is 
 function declareWinner () {
     document.body.classList.add("game-over");
+    // I don't really understand this part 
 }
 
 
--- Exercises and Homework/Homework 04/starter_code/level-one.js
+++ Exercises and Homework/Homework 04/starter_code/level-one.js
@@ -1,11 +1,27 @@
-/*
- * DOTS: Level One
- *
- */
-let score = 0;
-let ballEl = document.querySelector('.js-ball');
-let scoreEl = document.querySelector('.js-score');
+// Level 1
 
-function declareWinner() {
-  document.body.classList.add('game-over');
+// define what are score and ball element on the page. I don't understand why using El tho
+let score = 0; 
+let ballEl = document.querySelector(".js-ball");
+let scoreEl = document.querySelector (".js-score");
+
+console.log(ballEl)
+
+//define how the click is going to funtion
+ballEl.addEventListener("click", function() {
+    // define click will increase scores by 10
+    score += 10;
+    // define what condition should declare a winner. If the scores are over 100 then you decleare the winner
+    if (score < 100) {
+    scoreEl.innerText = score;
+    } else {
+    declareWinner();
+    }
+});
+
+// Define what fuction of decleare winner is 
+function declareWinner () {
+    document.body.classList.add("game-over");
+    // I don't really understand this part 
 }
+
